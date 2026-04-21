b50944a506fd7b49243eb4aae6beb10c1f2d6780
Facundo Agüero <fac.aguero94@gmail.com>
2026-04-21 02:16:37 -0300

    leer variables del formulario y extraer los valores del formulario con un escuchador de eventos

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ const user= document.getElementById("user");
 const password= document.getElementById("password");
 const message= document.getElementById("message");
 
-console.log(form, user, password, message);
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+   
+    const userValue = user.value.trim();
+    const passwordValue = password.value.trim();
+
+    console.log(userValue);
+    console.log(passwordValue);
+
+});
