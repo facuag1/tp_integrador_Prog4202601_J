39e93ae17b5e680feebf15d44b1efc14b52c8fac
Facundo Agüero <fac.aguero94@gmail.com>
2026-04-21 01:36:48 -0300

    creacion de pagina css y js, boceto de un login

---
+++ script.js
@@ -0,0 +1,6 @@
+const form= document.getElementById("form");
+const user= document.getElementById("user");
+const password= document.getElementById("password");
+const message= document.getElementById("message");
+
+console.log(form, user, password, message);
